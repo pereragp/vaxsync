dbb28e435440e82c2a37cdd08bdfcc032dce9dbb
Sinethma de Silva <sinethmakavithaki@gmail.com>
2025-09-21 16:39:56 +0530

    Vaccines added to the DB

---
+++ backend/src/routes/scheduleRoutes/vaccineRoutes.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { VaccinationController } from "../../controllers/scheduleController/vaccineController";
+import { authenticateToken } from "../../middleware/auth";
+import { body, param, query } from "express-validator";
+import { validateRequest } from "../../middleware/validation";
+import { create } from "domain";
+
+const router = express.Router();
+
+router.post("/", createVaccine);
+router.get(
+  "/",
+  authenticateToken,
+  async function getAllVaccines(req: any, res: any) {
+    return VaccinationController.getAllVaccines(req, res);
+  }
+);
+
+router.get(
+  "/:id",
+  authenticateToken,
+  async function getVaccineById(req: any, res: any) {
+    return VaccinationController.getVaccineById(req, res);
+  }
+);
+
+async function createVaccine(req: any, res: any) {
+  return VaccinationController.createVaccine(req, res);
+}
+
+export default router;
